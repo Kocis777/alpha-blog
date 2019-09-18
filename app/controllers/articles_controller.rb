e5fcbdacd6b432c450b80185a1e6dd12ebf977b2
Exequiel <exequielpc@gmail.com>
2019-09-18 17:30:15 +0800

    add signed in after signup and minor errors with restrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -63,5 +63,5 @@ class ArticlesController < ApplicationController
         flash[:danger] = "You can only edit or delete your own articles"
         redirect_to root_path
     end
-end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_user, except: [:new, :index, :show]
-    before_action :require_same_user, only: [:edit, :update, :destroy]  
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]  
 
     def new
         @user = User.new
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to Alpha Blog, #{@user.username}!"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -45,4 +46,11 @@ class UsersController < ApplicationController
     def set_user
         @user = User.find(params[:id])
     end
+
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "You can only edit or delete your own account"
+            redirect_to root_path
+        end
+      end
 end
