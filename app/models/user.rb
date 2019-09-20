aa864e89ffa632fe0b70aa32774d36058836cb12
Exequiel <exequielpc@gmail.com>
2019-09-20 13:06:18 +0800

    changes to the app adding tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
   before_save {self.email = email.downcase}
-  validates :username, presence: true, uniqueness: {case_sensitive:false}, length: {minimum: 3, maximum: 25}
+  validates :username, presence: true, uniqueness: {case_sensitive:false}, length: {minimum: 3, maximum: 50}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}, length: {maximum: 105}
   has_secure_password
