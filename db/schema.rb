d58cd45d2abb48bb29e4bad5284f85d320165738
Exequiel <exequielpc@gmail.com>
2019-09-23 14:22:08 +0800

    update all views with categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_064233) do
+ActiveRecord::Schema.define(version: 2019_09_23_053615) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
